879b52a53f25f6b0f8ad24fd25e9dfb50cde3cb7
Диана <Diana@MacBook-Pro-Aleksej.local>
2023-06-05 15:35:49 +0300

    fixed backend1

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,6 @@
 const TelegramBot = require('node-telegram-bot-api');
 const http = require('http');
-const express = require('express');
-const cors = require('cors');
+
 
 require('dotenv').config();
 const token = process.env.TOKEN || "токен бота узнаешь у botFathers"
@@ -9,10 +8,7 @@ const bot = new TelegramBot(token, {
   polling: true,
 });
 
-const app = express();
 
-app.use(express.json());
-app.use(cors());
 
 const chatId = process.env.CHATID || "id чата";
 //Узнать id чата bot.on('message', function (msg){console.log(msg.from.id)})
@@ -55,20 +51,4 @@ if( req.url === "/message/"){
 
 
 
-
-
-
-
-
-// Done! Congratulations on your new bot. You will find it at t.me/ImportantDay78_bot. You can now add a description, about section and profile picture for your bot, see /help for a list of commands. By the way, when you've finished creating your cool bot, ping our Bot Support if you want a better username for it. Just make sure the bot is fully operational before you do this.
-
-// Use this token to access the HTTP API:
-// 5668510786:AAHighnzpFZ7audlhdI06qpG1W5kxOioGsc
-// Keep your token secure and store it safely, it can be used by anyone to control your bot.
-
-// For a description of the Bot API, see this page: https://core.telegram.org/bots/api
-
-// Your user ID: 875245613
-// Current chat ID: 875245613
-// Forwarded from chat: -1001941601485
 
